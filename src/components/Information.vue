e6f0cae15ec1e5b24c7e7ba550af0c0ae22464cf
vphone <veronique.phone@gmail.com>
2021-04-04 00:08:34 +0200

    rename component

---
+++ src/components/Information.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <h1>{{this.title}}</h1>
+  <p>{{this.description}}</p>
+  <img :src="this.posterUrl"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Information',
+  props: {
+    title: String,
+    description: String,
+    posterUrl: String
+  }
+}
+</script>
