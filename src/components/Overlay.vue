70cbfb2423e701ea7828b10190794790d7b1521f
vphone <veronique.phone@gmail.com>
2021-04-03 23:46:58 +0200

    create new component overlay

---
+++ src/components/Overlay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <h1>{{this.title}}</h1>
+  <p>{{this.description}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Overlay',
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
